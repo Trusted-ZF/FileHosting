baf8505b74d0331f5bd13fc81042fad2b883763c
Zach <118992216+Trusted-ZF@users.noreply.github.com>
2024-08-27 13:28:23 +0100

    Update Invoke-TrustedITNotification.ps1
    Adjusted methodology, as the notification app and handler require admin access to be created.

--- Invoke-TrustedITNotification.ps1
+++ Invoke-TrustedITNotification.ps1
@@ -15,6 +15,11 @@ param (
 
 
 
+# Validate parameters
+$Preset = $Preset.ToLower()
+
+
+
 # Create working folder and start logging
 $WorkingFolder = "C:\ProgramData\Trusted IT"
 if (-not (Test-Path -Path $WorkingFolder)) {
@@ -30,148 +35,15 @@ Add-Content -Path $LogFile -Value "New notification run started."
 
 
 
-# Install required BurntToast module
-if (-not (Get-Module -Name "BurntToast" -ListAvailable)) {
-    try {
-        Add-Content -Path $LogFile -Value "Module BurntToast is not available. Installing..."
-
-        Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-        Install-Module -Name "BurntToast" -Scope CurrentUser -AcceptLicense:$true | Out-Null
-        Set-PSRepository -Name "PSGallery" -InstallationPolicy Untrusted
-    
-        Add-Content -Path $LogFile -Value "Done."
-    }
-    catch {
-        Add-Content -Path $LogFile -Value "[ERROR]: Could not install BurntToast module: $($_.Exception.Message)"
-    }
-}
-
-
-
-# Hero image
+# Set image locations
 $HeroImage = "$WorkingFolder\Hero-TrustedITLogo.jpg"
-if (-not (Test-Path -Path $HeroImage)) {
-    try {
-        Add-Content -Path $LogFile -Value "Downloading hero image..."
-        $HeroURL = "https://raw.githubusercontent.com/Trusted-ZF/FileHosting/main/TrustedITLogo_Hero.jpg"
-        Invoke-WebRequest -Uri $HeroURL -OutFile $HeroImage
-        Add-Content -Path $LogFile -Value "Done."
-    }
-    catch {
-        Add-Content -Path $LogFile -Value "[ERROR]: Could not download hero image: $($_.Exception.Message)"
-    }
-}
-
-
-
-# Icon image
 $IconImage = "$WorkingFolder\Icon-TrustedITMSP.png"
-if (-not (Test-Path -Path $IconImage)) {
-    try {
-        Add-Content -Path $LogFile -Value "Downloading AppIcon image..."
-        $IconURL = "https://raw.githubusercontent.com/Trusted-ZF/FileHosting/main/TrustedITMSP_Icon.png"
-        Invoke-WebRequest -Uri $IconURL -OutFile $IconImage
-        Add-Content -Path $LogFile -Value "Done."
-    }
-    catch {
-        Add-Content -Path $LogFile -Value "[ERROR]: Could not download AppIcon image: $($_.Exception.Message)"
-    }
-}
-
-
-
-# Notification App image
-$NotifyIcon = "$WorkingFolder\AppIcon-TrustedIT.ico"
-if (-not (Test-Path -Path $NotifyIcon)) {
-    try {
-        Add-Content -Path $LogFile -Value "Downloading NotifyIcon image..."
-        $AppURL	= "https://raw.githubusercontent.com/Trusted-ZF/ToastNotifications/main/TrustedITIcon.ico"
-        Invoke-WebRequest -Uri $AppURL -OutFile $NotifyIcon
-        Add-Content -Path $LogFile -Value "Done."
-    }
-    catch {
-        Add-Content -Path $LogFile -Value "[ERROR]: Could not download NotifyIcon image: $($_.Exception.Message)"
-    }
-}
-
-
-
-# Clear old reboot handlers
-try {
-    New-PSDrive -Name "HKCR" -PSProvider Registry -Root HKEY_CLASSES_ROOT -ErrorAction SilentlyContinue | Out-Null
-}
-catch {
-    Add-Content -Path $LogFile -Value "[ERROR]: Could not create HKCR PSDrive: $($_.Exception.Message)"
-}
-
-try {
-    Add-Content -Path $LogFile -Value "Removing old reboot handlers, if present..."
-    $HandlerPaths = @("HKCR:\TrustedIT-Reboot", "HKCR:\TrustedITReboot")
-    $HandlerPaths | ForEach-Object {
-        Remove-Item -Path $_ -Recurse -Force -ErrorAction SilentlyContinue
-    }
-    Add-Content -Path $LogFile -Value "Done."
-}
-catch {
-    Add-Content -Path $LogFile -Value "[ERROR]: Could not remove old reboot handlers."
-}
-
-
-
-# Create reboot handler
-try {
-    Add-Content -Path $LogFile -Value "Creating new reboot handler..."
-    $HandlerPath = "HKCR:\TrustedIT.Reboot"
-    $Handler = Get-Item $HandlerPath -ErrorAction SilentlyContinue
-    if (-not $Handler) {
-        New-Item -Path $HandlerPath -Force
-        Set-ItemProperty -Path $HandlerPath -Name "(DEFAULT)" -Value "url:TrustedIT.Reboot" -Force
-        Set-ItemProperty -Path $HandlerPath -Name "URL Protocol" -Value "" -Force
-        New-ItemProperty -Path $HandlerPath -Name "EditFlags" -PropertyType DWORD -Value 2162688
-        New-Item -Path "$HandlerPath\Shell\Open\Command" -Force
-        Set-ItemProperty "$HandlerPath\Shell\Open\Command" -Name "(DEFAULT)" -Value "C:\Windows\System32\shutdown.exe -r -t 00" -Force
-    }
-    Add-Content -Path $LogFile -Value "Done."
-}
-catch {
-    Add-Content -Path $LogFIle -Value "[ERROR]: Could not create new reboot handler: $($_.Exception.Message)"
-}
-
-
-
-# Create notification handler app
-try {
-    Add-Content -Path $LogFile -Value "Creating new notification handler..."
-    $RegPath = "HKCR:\AppUserModelId"
-    $AppIdPath = "$RegPath\TrustedIT.Notifications"
-    if (-not (Test-Path -Path $AppIdPath)) {
-        New-Item -Path $RegPath -Name "TrustedIT.Notifications" -Force | Out-Null
-        New-BTAppId -AppId "TrustedIT.Notifications"
-    }
-    Add-Content -Path $LogFile -Value "1/3 - Created registry paths."
-
-    $AppDisplayName = (Get-ItemProperty -Path $AppIdPath -Name DisplayName -ErrorAction SilentlyContinue).DisplayName
-    if ($AppDisplayName -ne "Trusted IT") {
-        New-ItemProperty -Path $AppIdPath -Name DisplayName -Value "Trusted IT" -PropertyType String -Force | Out-Null
-    }
-    Add-Content -Path $LogFile -Value "2/3 - Set app display name."
-
-    $AppIconPath = (Get-ItemProperty -Path $AppIdPath -Name IconUri -ErrorAction SilentlyContinue).IconUri
-    if ($AppIconPath -ne "$NotifyIcon") {
-        New-ItemProperty -Path $AppIdPath -Name IconUri -Value $NotifyIcon -PropertyType String -Force | Out-Null
-    }
-    Add-Content -Path $LogFile -Value "3/3 - Set app icon path."
-    Add-Content -Path $LogFile -Value "Done."
-}
-catch {
-    Add-Content -Path $LogFile -Value "[ERROR]: Could not create new notification handler: $($_.Exception.Message)"
-}
 
 
 
 # Handle templates
 switch ($Preset) {
-    "Reboot"		{
+    "reboot"		{
         try {
             $5Min		= New-BTSelectionBoxItem -Id 5 -Content "5 Minutes"
             $10Min		= New-BTSelectionBoxItem -Id 10 -Content "10 Minutes"
@@ -202,7 +74,7 @@ switch ($Preset) {
             Add-Content -Path $LogFile -Value "[ERROR]: Could not submit notification: $($_.Exception.Message)"
         }
     }
-    "ServerProblem" {
+    "serverproblem" {
         try {
             $BTText     = New-BTText -Text "We're aware of a problem with the server and we're working on it. We'll update you as soon as possible."
             $BTHero     = New-BTImage -Source $HeroImage -HeroImage
