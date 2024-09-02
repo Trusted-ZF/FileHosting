2e7ddb467afae39d5582968691718819bcac1ca4
Zach <118992216+Trusted-ZF@users.noreply.github.com>
2024-09-02 09:36:07 +0100

    Update Invoke-TrustedITNotification.ps1

--- Invoke-TrustedITNotification.ps1
+++ Invoke-TrustedITNotification.ps1
@@ -16,7 +16,7 @@ param (
 
 # Validate parameters
 $Preset = $Preset.ToLower()
-$AcceptedPresets = @("reboot", "serverproblem")
+$AcceptedPresets = @("reboot", "rebootforpatching", "serverproblem", "startpatchjob")
 if ($Preset -notin $AcceptedPresets) {
     exit "Invalid preset."
 }
@@ -100,6 +100,36 @@ switch ($Preset) {
             Add-Content -Path $LogFile -Value "[ERROR]: Could not submit notification: $($_.Exception.Message)"
         }
     }
+    "rebootforpatching" {
+        try {
+            $5Min		= New-BTSelectionBoxItem -Id 5 -Content "5 Minutes"
+            $10Min		= New-BTSelectionBoxItem -Id 10 -Content "10 Minutes"
+            $1Hour		= New-BTSelectionBoxItem -Id 60 -Content "1 Hour"
+            $4Hour		= New-BTSelectionBoxItem -Id 240 -Content "4 Hours"
+            $1Day		= New-BTSelectionBoxItem -Id 1440 -Content "1 Day"
+            $Items		= $5Min, $10Min, $1Hour, $4Hour, $1Day
+            $Selection	= New-BTInput -Id "SnoozeTime" -DefaultSelectionBoxItemId 10 -Items $Items
+
+            $BTText1	= New-BTText -Text "Security updates have been installed."
+            $BTText2	= New-BTText -Text "Please restart your computer when convenient."
+            $BTHero		= New-BTImage -Source $HeroImage -HeroImage
+            $BTIcon		= New-BTImage -Source $IconImage -AppLogoOverride -Crop Circle
+            $BTAudio	= New-BTAudio -Source ms-winsoundevent:Notification.Looping.Call2
+
+            $Action		= New-BTButton -Content "Restart now" -Arguments "TrustedIT.Reboot:" -ActivationType Protocol
+            $Dismiss	= New-BTButton -Content "Snooze" -Snooze -Id "SnoozeTime"
+            $BTHolder	= New-BTAction -Buttons $Dismiss, $Action -Inputs $Selection
+
+            $BTBinding	= New-BTBinding -Children $BTText1, $BTText2 -HeroImage $BTHero -AppLogoOverride $BTIcon
+            $BTVisual	= New-BTVisual -BindingGeneric $BTBinding
+            $BTContent	= New-BTContent -Visual $BTVisual -Audio $BTAudio -Duration Long -Scenario IncomingCall -Actions $BTHolder
+            Submit-BTNotification -Content $BTContent -AppId "TrustedIT.Notifications"
+            Add-Content -Path $LogFile -Value "Notification submitted successfully."
+        }
+        catch {
+            Add-Content -Path $LogFile -Value "[ERROR]: Could not submit notification: $($_.Exception.Message)"
+        }
+    }
     "serverproblem" {
         try {
             $BTText     = New-BTText -Text "We're aware of a problem with the server and we're working on it. We'll update you as soon as possible."
@@ -114,6 +144,36 @@ switch ($Preset) {
             $BTBinding  = New-BTBinding -Children $BTText -HeroImage $BTHero -AppLogoOverride $BTIcon
             $BTVisual   = New-BTVisual -BindingGeneric $BTBinding
             $BTContent  = New-BTContent -Visual $BTVisual -Audio $BTAudio -Duration Long -Scenario IncomingCall -Actions $BTHolder
+            Submit-BTNotification -Content $BTContent -AppId "TrustedIT.Notifications"
+            Add-Content -Path $LogFile -Value "Notification submitted successfully."
+        }
+        catch {
+            Add-Content -Path $LogFile -Value "[ERROR]: Could not submit notification: $($_.Exception.Message)"
+        }
+    }
+    "startpatchjob" {
+        try {
+            $5Min		= New-BTSelectionBoxItem -Id 5 -Content "5 Minutes"
+            $10Min		= New-BTSelectionBoxItem -Id 10 -Content "10 Minutes"
+            $1Hour		= New-BTSelectionBoxItem -Id 60 -Content "1 Hour"
+            $4Hour		= New-BTSelectionBoxItem -Id 240 -Content "4 Hours"
+            $1Day		= New-BTSelectionBoxItem -Id 1440 -Content "1 Day"
+            $Items		= $5Min, $10Min, $1Hour, $4Hour, $1Day
+            $Selection	= New-BTInput -Id "SnoozeTime" -DefaultSelectionBoxItemId 10 -Items $Items
+
+            $BTText1    = New-BTText -Text "Security updates are available."
+            $BTText2    = New-BTText -Text "Please confirm if they can be installed now, or later (up to 24 hours)."
+            $BTHero		= New-BTImage -Source $HeroImage -HeroImage
+            $BTIcon		= New-BTImage -Source $IconImage -AppLogoOverride -Crop Circle
+            $BTAudio	= New-BTAudio -Source ms-winsoundevent:Notification.Looping.Call2
+
+            $Action		= New-BTButton -Content "Install now" -Arguments "none"
+            $Dismiss	= New-BTButton -Content "Install later" -Snooze -Id "SnoozeTime"
+            $BTHolder   = New-BTAction -Buttons $Dismiss, $Action
+
+            $BTBinding	= New-BTBinding -Children $BTText1, $BTText2 -HeroImage $BTHero -AppLogoOverride $BTIcon
+            $BTVisual	= New-BTVisual -BindingGeneric $BTBinding
+            $BTContent	= New-BTContent -Visual $BTVisual -Audio $BTAudio -Duration Long -Scenario IncomingCall -Actions $BTHolder
             Submit-BTNotification -Content $BTContent -AppId "TrustedIT.Notifications"
             Add-Content -Path $LogFile -Value "Notification submitted successfully."
         }
